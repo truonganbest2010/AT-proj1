a25054148bf40e05588c6ef90d2b9f1b050f3950
AnTruong <truonganbest2010@gmail.com>
2020-09-15 01:20:24 -0500

    add keyboard

--- view/WordGuessGamePanel.java
+++ view/WordGuessGamePanel.java
@@ -5,12 +5,16 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 
+import controller.AlphabetsListener;
+
 public class WordGuessGamePanel {
     
     private JFrame window;
-    private JTextField gamekey = new JTextField();
-    private JTextField guesskey = new JTextField();
+    private JTextField gameKey = new JTextField();
+    private JTextField guessKey = new JTextField();
     private DrawingCanvas canvas = new DrawingCanvas(this);
+    private JButton[] characterKey = new JButton[26];
+    private JButton newKey = new JButton("New");
 
     public WordGuessGamePanel(JFrame window){
         this.window = window;
@@ -23,25 +27,55 @@ public class WordGuessGamePanel {
         
         JPanel northPanel = new JPanel();
         northPanel.setLayout(new GridLayout(2,1));
-        TitledBorder northTitle = BorderFactory.createTitledBorder("Key");
-        northPanel.setBorder(northTitle);
+        //TitledBorder northTitle = BorderFactory.createTitledBorder("Key");
+        //northPanel.setBorder(northTitle);
         cp.add(BorderLayout.NORTH, northPanel);
 
-        gamekey.setEnabled(false);
-        guesskey.setEnabled(false);
-        northPanel.add(gamekey);
-        northPanel.add(guesskey);
+        gameKey.setEnabled(false);
+        guessKey.setEnabled(false);
+        northPanel.add(gameKey);
+        northPanel.add(guessKey);
 
         cp.add(BorderLayout.CENTER, canvas);
 
         JPanel southPanel = new JPanel();
         southPanel.setLayout(new GridLayout(4, 7));
-        TitledBorder southTitle = BorderFactory.createTitledBorder("Input");
-        southPanel.setBorder(southTitle);
+        //TitledBorder southTitle = BorderFactory.createTitledBorder("Input");
+        //southPanel.setBorder(southTitle);
         cp.add(BorderLayout.SOUTH, southPanel);
-        
 
 
+        AlphabetsListener eventListener = new AlphabetsListener(this);
+        int i = 0;
+        for (char c = 'a' ; c < 'z'; c++){
+            characterKey[i] = new JButton("" + c);
+            southPanel.add(characterKey[i]);
+            characterKey[i].addActionListener(eventListener);
+            i++;
+        } i = 0;
 
+        southPanel.add(newKey);
+        newKey.addActionListener(eventListener);
+
+    }
+
+    public JTextField getGameKey(){
+        return gameKey;
+    }
+
+    public JTextField getGuessKey(){
+        return guessKey;
+    }
+
+    public DrawingCanvas getCanvas(){
+        return canvas;
+    }
+
+    public JButton[] getCharacterKey(){
+        return characterKey;
+    }
+
+    public JButton getNewKey(){
+        return newKey;
     }
 }
